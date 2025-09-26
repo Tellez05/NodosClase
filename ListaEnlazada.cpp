e706537a1d472e146660eabca22ac702e289f92d
Tellez05 <A01640907@tec.mx>
2025-09-25 18:22:59 -0600

    El pepe

--- ListaEnlazada.cpp
+++ ListaEnlazada.cpp
@@ -17,3 +17,27 @@ int MyLinkedlist::first(){
     return this->Head ->Data;
 }
 
+int MyLinkedlist::last(){
+    return this->tail->Data;
+}
+
+int MyLinkedlist::Getat(int pos){
+    int i {0};
+    ListaEnlazada* current {this->Head}; 
+    while(i < pos){
+        current = current -> Next; 
+    i++;    
+    }
+    return current->Data; 
+    
+
+}
+
+void MyLinkedlist::insertFirst(int data){
+    ListaEnlazada* Nuevo= new ListaEnlazada(data,this->Head);
+    Head  =Nuevo;
+    size++; 
+    if(size == 1){
+        tail = Nuevo; 
+    }
+}
